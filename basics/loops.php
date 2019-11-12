45d698d2e68d5145ea85afeae2f2fe4c2756e5b2
luana monteiro <luanamonteiropereira95@gmail.com>
2019-11-12 18:41:35 -0300

    criando uma tabela usando loop

--- basics/loops.php
+++ basics/loops.php
@@ -3,19 +3,47 @@ $contador = 1;
 
 echo ">>>> WHILE <<<<" . PHP_EOL;
 while ($contador <= 5) {
-    echo "#$contador" . PHP_EOL;
+    echo "$contador - " ;
     $contador ++;
 }
+
+
+echo PHP_EOL . ">>>> DO WHILE <<<<" . PHP_EOL;
+$i = 0;
+do {
+    echo "$i - ";
+    $i++;
+} while ($i < 5);
+
  
 echo PHP_EOL . ">>>> FOR <<<<" . PHP_EOL;
 for($i=0; $i<15; $i++){
-    if($i%2==0){
+    if($i == 1 || $i == 4){
         continue;
     }else if($i==7){
         break;
     }    
     else{
-        echo "# $i" . PHP_EOL;
+        echo "$i - ";
     }   
 }
+
+
+echo PHP_EOL;
 ?>
+
+
+<!-- criando uma tabela usando loop 'for' -->
+<table id="grid">
+<?php
+	for( $linha=0; $linha<10; $linha++ )
+	{
+		echo '<tr>';
+		for( $coluna=0; $coluna<4; $coluna++ )
+		{
+			echo " <td>$linha-$coluna</td> ";
+		}
+		echo '</tr>' . PHP_EOL;
+	}
+?>
+</table>
