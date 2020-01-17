5710f4f2f6d7eee28b0c365ebb21303d9fd49f95
luana monteiro <luanamonteiropereira95@gmail.com>
2020-01-17 17:27:51 -0300

    arquivo de configuração

--- catalogo/src/class/Connection.php
+++ catalogo/src/class/Connection.php
@@ -1,7 +1,9 @@
 <?php
+require_once 'config.php';
+
 class Connection{
     public static function getConnection(){
-        $conn = new PDO('pgsql:host=localhost;port=5432;dbname=persiann;user=persiann;password=persiann');
+        $conn = new PDO(DB_DRIVE .':host='.DB_HOSTNAME.';port=5432;dbname='.DB_DATABASE.';user='.DB_USERNAME.';password=' .DB_PASSWORD);
         return $conn;
     }
 }
---
+++ catalogo/src/class/config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_DRIVE', 'pgsql');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_DATABASE', 'persiann');
+define('DB_USERNAME', 'persiann');
+define('DB_PASSWORD', 'persiann');
