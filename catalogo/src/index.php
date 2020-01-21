307f088effe5a6387ebf72edb30c3452647d2c25
luana monteiro <luanamonteiropereira95@gmail.com>
2020-01-21 13:04:00 -0300

    refactoring youtube api code

--- catalogo/src/index.php
+++ catalogo/src/index.php
@@ -150,16 +150,7 @@ $lista = $disco->selectAll();
             $(".song").html('<i class="far fa-play-circle"></i>');
         }
 
-
-        // 2. This code loads the IFrame Player API code asynchronously.
-        var tag = document.createElement('script');
-
-        tag.src = "https://www.youtube.com/iframe_api";
-        var firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-        // 3. This function creates an <iframe> (and YouTube player)
-        //    after the API code downloads.
+        /* 3. This function creates an <iframe> (and YouTube player) after the API code downloads.*/
         var player;
 
         function onYouTubeIframeAPIReady() {
